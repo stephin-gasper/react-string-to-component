43c9593dfd1540f05d8333dd2f217f117be9df10
stephin-gasper <stephin.gasper21@gmail.com>
2018-11-11 23:22:24 +0530

    :sparkles: Imported components using react-loadable - Used single import statement - Removed getComponent function as it was unnessary

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
 import React, { Component } from "react";
 import shortid from "shortid";
 import Axios from "axios";
-import NullComponent from "./sample-components/NullComponent";
-
+import Loadable from "react-loadable";
+// import NullComponent from "./sample-components/NullComponent";
+import Loader from "./Loader";
 class App extends Component {
   state = {
     components: []
   };
-  /**
-   * Used for importing components dyanamically by string provided.
-   * 
-   * @param {string} componentStr - Component filename
-   * @returns {object} React Component
-   * @memberof App
-   */
-  getComponent = async componentStr => {
-    console.log(`Loading ${componentStr} component...`);
-    try {
-      const FetchedComponent = await import(`./sample-components/${componentStr}.js`);
-      return <FetchedComponent.default key={shortid.generate()} />;
-    } catch (error) {
-      return <NullComponent key={shortid.generate()} />;
-    }
-  };
   componentDidMount = () => {
     Axios.get("http://5be5853348c1280013fc3d63.mockapi.io/cc/components")
       .then(response => {
-        const componentList = response.data.map(el =>
-          this.getComponent(el.name)
+        const loadableComponents = response.data.map(el =>
+          Loadable({
+            loader: () => import(`./sample-components/${el.name}`),
+            loading: Loader
+          })
         );
-        Promise.all(componentList).then(list => {
-          this.setState({ components: list });
-        });
+        const componentList = loadableComponents.map(Item => (
+          <Item key={shortid.generate()} />
+        ));
+        this.setState({ components: componentList });
       })
       .catch(error => {
         console.log(error);
